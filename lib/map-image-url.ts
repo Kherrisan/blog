5c6078d7176a264222c61acd4ff9cd3a5c2fd4d5
Kherrisan <zdkscope@qq.com>
2022-04-25 14:23:36 +0800

    Mimi

--- lib/map-image-url.ts
+++ lib/map-image-url.ts
@@ -2,7 +2,21 @@ import { defaultMapImageUrl } from 'react-notion-x'
 
 export const mapImageUrl = defaultMapImageUrl
 
+const FILENAME_REGEX = /\/([\w-]+?)\/(\w+?)\.(png|jpeg|jpg|gif)/
+const ALI_OSS_REGEX = /https:\/\/[\w-]+?\.oss-cn-([a-z]+?)\.(aliyuncs\.com).+?\?/
+
 export const proxyMapImageUrl = (url, block): string => {
     const mappedUrl = defaultMapImageUrl(url, block);
-    return `https://biproxy.cdn.kherrisan.cn/image/${encodeURI(mappedUrl)}`;
+    let m = FILENAME_REGEX.exec(url);
+    if (m) {
+        console.log(`https://biproxy.cdn.kherrisan.cn/image${m[0]}`)
+        return `https://biproxy.cdn.kherrisan.cn/image${m[0]}?data=${Buffer.from(mappedUrl).toString('base64')}`;
+    }
+    url = decodeURIComponent(mappedUrl);
+    m = ALI_OSS_REGEX.exec(url);
+    if (m) {
+        console.log(`${m[0]}`)
+        return m[0];
+    }
+    return mappedUrl
 }
